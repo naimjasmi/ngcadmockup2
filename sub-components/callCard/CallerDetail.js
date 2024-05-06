9fc362f2c8000288101780ed3071e555880c1b9d
Your Name <you@example.com>
2024-05-06 12:38:52 +0800

    fix stack error adn remove unused components

--- sub-components/callCard/CallerDetail.js
+++ sub-components/callCard/CallerDetail.js
@@ -10,6 +10,21 @@ import { EventCode } from 'sub-components'
 
 const CallerDetail = () => {
   const hasMounted = useMounted();
+
+  const levelOptions = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' }
+];
+
+  const proximityOptions = [
+    { value: 'At the address', label: 'At the address' },
+    { value: 'One', label: 'One' },
+    { value: 'Two', label: 'Two' },
+    { value: 'Three', label: 'Three' }
+];
+
   const precintOptions = [
     { value: 'Precint2', label: 'Precint 2' },
     { value: 'Precint3', label: 'Precint 3' },
@@ -23,69 +38,127 @@ const CallerDetail = () => {
 
   return (
     <Row className="mb-8">
-      <Col xl={7} lg={7} md={12} xs={12}>
-        <EventCode />
-      </Col>
-      <Col xl={5} lg={5} md={12} xs={12}>
-        <Card>
-          {/* card body */}
+        <Card >
           <Card.Body>
-            {/* col */}
-            <div>
+            <div className="mb-6">
+              <h4 className="mb-1">Incident</h4>
+            </div>
+            <Row>
+              <Col xl={3} md={12} className="mb-3">
+                <Form.Control as={FormSelect} defaultselected="Always" id="proximity" options={proximityOptions} />
+              </Col>
+              <Col xl={7} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Business Place / Location (Administrative Division)" id="fullName" required />
+              </Col>
+              <Col xl={2} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Level" id="time" options={levelOptions} />
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={3} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="St. No" id="streetNo" required />
+              </Col>
+              <Col xl={6} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Street / Intersection" id="street" required />
+              </Col>
+              <Col xl={3} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Postal Code" id="postalCode" options={levelOptions} />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xl={2} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Precint" id="precint" options={levelOptions} />
+              </Col>
+              <Col xl={5} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="City" id="City" options={levelOptions} />
+              </Col>
+              <Col xl={2} md={6} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Province" id="province" options={levelOptions} />
+              </Col>
+              <Col xl={3} md={6} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Postal Code" id="postalCode" options={levelOptions} />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xl={4} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Phone No" id="phoneNo" required />
+              </Col>
+              <Col xl={2} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Phone Extension" id="phextension" required />
+              </Col>
+              <Col xl={6} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Crossing Street" id="crossingStreet" required />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xl={4} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Direction" id="direction" options={levelOptions} />
+              </Col>
+              <Col xl={2} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Risk Level" id="riskLevel" options={levelOptions} />
+              </Col>
+              <Col xl={6} md={12} className="mb-3">
+                <Form.Control type="text" placeholder="Property Usage" id="propertyUsage" required />
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={8} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Nature" id="nature" options={proximityOptions} />
+              </Col>
+              <Col xl={4} md={12} className="mb-3">
+                <Form.Control as={FormSelect} placeholder="Priority" id="priority" options={proximityOptions} />
+              </Col>
+
+              <hr className="my-6" />
+
               <div className="mb-6">
                 <h4 className="mb-1">Caller</h4>
               </div>
-              {hasMounted &&
-                <Form>
-                  <Row className="mb-3">
+              <Row className="mb-3">
 
-                    <Col sm={12} className="mb-3 mb-lg-0">
-                      <Form.Control type="text" placeholder="Caller Name" id="name" required />
-                    </Col>
-                  </Row>
-                  {/* row */}
-                  <Row className="mb-3">
-                    <Col sm={3}>
-                      <Form.Control type="text" placeholder="St. No" id="streetNo" required />
-                    </Col>
-                    <Col sm={5}>
-                      <Form.Control type="text" placeholder="Street / Intersection" id="intersection" required />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control type="text" placeholder="Apartment" id="apartment" required />
-                    </Col>
-                  </Row>
-                  <Row className="mb-3">
-                    <Col sm={3}>
-                      <Form.Control as={FormSelect} placeholder="Precint" id="precint" options={precintOptions} />
-                    </Col>
-                    <Col sm={5}>
-                      <Form.Control as={FormSelect} placeholder="City" id="city" options={cityOptions} />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control type="text" placeholder="Postal Code" id="postalCode" required />
-                    </Col>
-                  </Row>
-                  {/* row */}
-                  <Row className="mb-3">
-                    <Col md={12} xs={12}>
-                      <Form.Control type="text" placeholder="Enter Phone" id="phone" />
-                    </Col>
-                  </Row>
+                <Col sm={12} className="mb-3 mb-lg-0">
+                  <Form.Control type="text" placeholder="Caller Name" id="name" required />
+                </Col>
+              </Row>
+              {/* row */}
+              <Row className="mb-3">
+                <Col sm={3}>
+                  <Form.Control type="text" placeholder="St. No" id="streetNo" required />
+                </Col>
+                <Col sm={5}>
+                  <Form.Control type="text" placeholder="Street / Intersection" id="intersection" required />
+                </Col>
+                <Col sm={4}>
+                  <Form.Control type="text" placeholder="Apartment" id="apartment" required />
+                </Col>
+              </Row>
+              <Row className="mb-3">
+                <Col sm={3}>
+                  <Form.Control as={FormSelect} placeholder="Precint" id="precint" options={precintOptions} />
+                </Col>
+                <Col sm={5}>
+                  <Form.Control as={FormSelect} placeholder="City" id="city" options={cityOptions} />
+                </Col>
+                <Col sm={4}>
+                  <Form.Control type="text" placeholder="Postal Code" id="postalCode" required />
+                </Col>
+              </Row>
+              {/* row */}
+              <Row className="mb-3">
+                <Col md={12} xs={12}>
+                  <Form.Control type="text" placeholder="Enter Phone" id="phone" />
+                </Col>
+              </Row>
 
-                  <Row className="align-items-center">
-                    <Col xl={3} md={12}>
-                      <Button variant="primary" type="submit">Create</Button>
-                    </Col>
-
-                  </Row>
-                </Form>
-              }
-            </div>
+              <Col xl={3} md={12}>
+                <Button variant="primary" type="submit">Create</Button>
+              </Col>
+            </Row>
           </Card.Body>
         </Card>
-
-      </Col>
     </Row>
   )
 }
--- sub-components/callCard/Geolocation.js
+++ sub-components/callCard/Geolocation.js
@@ -9,86 +9,20 @@ import useMounted from 'hooks/useMounted';
 import { EventCode } from 'sub-components'
 
 const Geolocation = () => {
-  const hasMounted = useMounted();
-  const countryOptions = [
-    { value: 'Malaysia', label: 'Malaysia' },
-  ];
-  const precintOptions = [
-    { value: 'Precint2', label: 'Precint 2' },
-    { value: 'Precint3', label: 'Precint 3' },
-    { value: 'Precint4', label: 'Precint 4' },
-  ];
-  const cityOptions = [
-    { value: 'ampang', label: 'AMPANG (SL)' },
-    { value: 'pj', label: 'PETALING JAYA (SL)' },
-    { value: 'puchong', label: 'PUCHONG (SL)' },
-  ];
 
   return (
     <Row className="mb-8">
-      <Col xl={7} lg={7} md={12} xs={12}>
-        <EventCode />
-      </Col>
-      <Col xl={5} lg={5} md={12} xs={12}>
-        <Card>
-          {/* card body */}
-          <Card.Body>
-            {/* col */}
-            <div>
-              <div className="mb-6">
-                <h4 className="mb-1">Caller</h4>
-              </div>
-              {hasMounted &&
-                <Form>
-                  <Row className="mb-3">
+      <div>
+        <div className="mb-6">
+          <h4 className="mb-1">Geolocation</h4>
+        </div>
+        <Row className="mb-3">
 
-                    <Col sm={12} className="mb-3 mb-lg-0">
-                      <Form.Control type="text" placeholder="Caller Name" id="name" required />
-                    </Col>
-                  </Row>
-                  {/* row */}
-                  <Row className="mb-3">
-                    <Col sm={3}>
-                      <Form.Control type="text" placeholder="St. No" id="streetNo" required />
-                    </Col>
-                    <Col sm={5}>
-                      <Form.Control type="text" placeholder="Street / Intersection" id="intersection" required />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control type="text" placeholder="Apartment" id="apartment" required />
-                    </Col>
-                  </Row>
-                  <Row className="mb-3">
-                    <Col sm={3}>
-                      <Form.Control as={FormSelect} placeholder="Precint" id="precint" options={precintOptions} />
-                    </Col>
-                    <Col sm={5}>
-                      <Form.Control as={FormSelect} placeholder="City" id="city" options={cityOptions} />
-                    </Col>
-                    <Col sm={4}>
-                      <Form.Control type="text" placeholder="Postal Code" id="postalCode" required />
-                    </Col>
-                  </Row>
-                  {/* row */}
-                  <Row className="mb-3">
-                    <Col md={12} xs={12}>
-                      <Form.Control type="text" placeholder="Enter Phone" id="phone" />
-                    </Col>
-                  </Row>
-
-                  <Row className="align-items-center">
-                    <Col xl={3} md={12}>
-                      <Button variant="primary" type="submit">Create</Button>
-                    </Col>
-
-                  </Row>
-                </Form>
-              }
-            </div>
-          </Card.Body>
-        </Card>
-
-      </Col>
+          <Col sm={12} className="mb-3 mb-lg-0">
+            {/* add the map here*/}
+          </Col>
+        </Row>
+      </div>
     </Row>
   )
 }
