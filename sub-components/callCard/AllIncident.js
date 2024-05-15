c77132822f1913d967445a5a6a8cab9c386d89b5
Your Name <you@example.com>
2024-05-15 17:06:26 +0800

    add dummy data in incident table

--- sub-components/callCard/AllIncident.js
+++ sub-components/callCard/AllIncident.js
@@ -2,6 +2,78 @@ import React from 'react';
 import { Card, Table, Badge, Button } from 'react-bootstrap';
 
 const AllIncident = ({ incidentDataList }) => {
+    // Dummy incidents
+    const dummyIncidents = [
+        {
+          callcardNo: 'ABC123',
+          nature: 'Fire',
+          streetNo: '123',
+          level: 'Level 1',
+          street: 'Main Street',
+          precint: 'Precint2',
+          postalCode: '12345',
+          City: 'Ampang',
+          province: 'Kuala Lumpur',
+          direction: 'North',
+          riskLevel: 'High',
+          priority: 'Very High',
+          name: 'Halimah Ja',
+          phoneNo: '123-456-7890'
+        },
+        {
+          callcardNo: 'DEF456',
+          nature: 'Robbery',
+          streetNo: '456',
+          level: 'Level 2',
+          street: 'First Avenue',
+          precint: 'Precint3',
+          postalCode: '54321',
+          City: 'Petaling Jaya',
+          province: 'Selangor',
+          direction: 'South',
+          riskLevel: 'Moderate',
+          priority: 'High',
+          name: 'Safick Salleh',
+          phoneNo: '987-654-3210'
+        },
+        {
+          callcardNo: 'GHI789',
+          nature: 'Assault',
+          streetNo: '789',
+          level: 'Level 3',
+          street: 'Second Street',
+          precint: 'Precint4',
+          postalCode: '67890',
+          City: 'Puchong',
+          province: 'Selangor',
+          direction: 'East',
+          riskLevel: 'Low',
+          priority: 'Medium',
+          name: 'Dwayne Pebble',
+          phoneNo: '111-222-3333'
+        },
+        {
+          callcardNo: 'JKL012',
+          nature: 'Traffic Accident',
+          streetNo: '012',
+          level: 'Level 4',
+          street: 'Third Avenue',
+          precint: 'Precint5',
+          postalCode: '09876',
+          City: 'Kuala Lumpur',
+          province: 'Kuala Lumpur',
+          direction: 'West',
+          riskLevel: 'Very High',
+          priority: 'Very High',
+          name: 'Raju Kumar Kamir',
+          phoneNo: '444-555-6666'
+        }
+      ];
+      
+
+    // Merge dummy incidents with existing incidents
+    const mergedIncidents = [...incidentDataList, ...dummyIncidents];
+
     return (
         <Card className="mb-4">
             <Card.Body>
@@ -25,17 +97,17 @@ const AllIncident = ({ incidentDataList }) => {
                             </tr>
                         </thead>
                         <tbody>
-                            {incidentDataList.map((incident, index) => (
+                            {mergedIncidents.map((incident, index) => (
                                 <tr key={index}>
                                     <td className="align-middle">{incident.callcardNo}</td>
                                     <td className="align-middle">{incident.nature}</td>
                                     <td className="align-middle">
                                         {`No. ${incident.streetNo},
-                     ${incident.level} ${incident.street}, 
-                     ${incident.precint}, 
-                     ${incident.postalCode}, 
-                     ${incident.City}, 
-                     ${incident.province}`}
+                                        ${incident.level} ${incident.street}, 
+                                        ${incident.precint}, 
+                                        ${incident.postalCode}, 
+                                        ${incident.City}, 
+                                        ${incident.province}`}
                                     </td>
                                     <td className="align-middle">{incident.direction}</td>
                                     <td className="align-middle">
