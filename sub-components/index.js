e6264cf0ce2e5df5fc67921c3a0e9c6a6da7c1b6
Your Name <you@example.com>
2024-05-21 11:53:07 +0800

    add use client

--- sub-components/index.js
+++ sub-components/index.js
@@ -2,7 +2,7 @@
  * The folder sub-components contains sub component of all the pages,
  * so here you will find folder names which are listed in root pages.
  */
-
+'use client'
 // sub components for /pages/dashboard
 import ActiveProjects from 'sub-components/dashboard/ActiveProjects';
 import TasksPerformance from 'sub-components/dashboard/TasksPerformance';
