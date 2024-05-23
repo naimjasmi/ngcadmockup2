0e6198b07ab2f72c27d0cffe70f408ed37597d33
Your Name <you@example.com>
2024-05-23 10:57:30 +0800

    minor changes for deployment error

--- app/(dashboard)/callCard/page.js
+++ app/(dashboard)/callCard/page.js
@@ -1,6 +1,5 @@
 'use client'
 import React, { useState } from 'react';
-import PropTypes from 'prop-types'; // Import prop-types for type checking
 import { Col, Row, Container } from 'react-bootstrap';
 import { CallerDetail, AllIncident, Comment, Resource } from 'sub-components'; // Adjust the path as needed
 
@@ -13,12 +12,10 @@ const CallCard = () => {
   };
 
   const handleFormSubmit = (formData) => {
-    // Set caller form data
-    const updatedFormData = { ...formData, resource: resourceFieldValue }; // Include resource field
+    const updatedFormData = { ...formData, resource: resourceFieldValue };
     setIncidentDataList([...incidentDataList, updatedFormData]);
   };
 
-  // Ensure this component is only rendered on the client-side
   if (typeof window === 'undefined') {
     return null;
   }
@@ -30,30 +27,19 @@ const CallCard = () => {
           <Comment />
         </Col>
         <Col xl={6} lg={6} md={6} xs={12}>
-          {/* Pass handleFormSubmit as a prop */}
           <CallerDetail onFormSubmit={handleFormSubmit} resourceFieldValue={resourceFieldValue} />
         </Col>
         <Col xl={4} lg={12} md={12} xs={12}>
-          {/* Pass updateResourceField as a prop */}
           <Resource onUpdateResource={updateResourceField} />
         </Col>
       </Row>
       <Row className="justify-content-center">
         <Col xl={12} lg={12} md={12} xs={12}>
-          {/* Pass incidentData and callerFormData to AllIncident */}
           <AllIncident incidentDataList={incidentDataList} resourceFieldValue={resourceFieldValue} />
         </Col>
       </Row>
     </Container>
   );
-};
-
-// Prop types for better type checking
-CallCard.propTypes = {
-  incidentDataList: PropTypes.array,
-  resourceFieldValue: PropTypes.string,
-  handleFormSubmit: PropTypes.func,
-  updateResourceField: PropTypes.func,
 };
 
 export default CallCard;
