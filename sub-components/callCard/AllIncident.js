88d7220e4fe333f9592354879e42d747c500a622
Your Name <you@example.com>
2024-05-30 11:08:56 +0800

    add dummy data, add space between button

--- sub-components/callCard/AllIncident.js
+++ sub-components/callCard/AllIncident.js
@@ -10,71 +10,76 @@ const AllIncident = ({ incidentDataList, onCallCardClick }) => {
     };
     const dummyIncidents = [
         {
-          callcardNo: 'ABC123',
-          nature: 'Fire',
-          streetNo: '123',
-          level: 'Level 1',
-          street: 'Main Street',
-          precint: 'Precint2',
-          postalCode: '12345',
-          City: 'Ampang',
-          province: 'Kuala Lumpur',
-          direction: 'North',
-          riskLevel: 'High',
-          priority: 'Very High',
-          name: 'Halimah Ja',
-          phoneNo: '123-456-7890'
+            callcardNo: 'ABC123',
+            nature: 'Fire',
+            streetNo: '123',
+            level: 'Level 1',
+            street: 'Main Street',
+            precint: 'Precint2',
+            postalCode: '12345',
+            City: 'Ampang',
+            province: 'Kuala Lumpur',
+            direction: 'North',
+            riskLevel: 'High',
+            priority: 'Very High',
+            name: 'Halimah Ja',
+            phoneNo: '123-456-7890',
+            resource: 'PO 245' // Added resource field
         },
         {
-          callcardNo: 'DEF456',
-          nature: 'Robbery',
-          streetNo: '456',
-          level: 'Level 2',
-          street: 'First Avenue',
-          precint: 'Precint3',
-          postalCode: '54321',
-          City: 'Petaling Jaya',
-          province: 'Selangor',
-          direction: 'South',
-          riskLevel: 'Moderate',
-          priority: 'High',
-          name: 'Safick Salleh',
-          phoneNo: '987-654-3210'
+            callcardNo: 'DEF456',
+            nature: 'Robbery',
+            streetNo: '456',
+            level: 'Level 2',
+            street: 'First Avenue',
+            precint: 'Precint3',
+            postalCode: '54321',
+            City: 'Petaling Jaya',
+            province: 'Selangor',
+            direction: 'South',
+            riskLevel: 'Moderate',
+            priority: 'High',
+            name: 'Safick Salleh',
+            phoneNo: '987-654-3210',
+            resource: 'MEDIC 405' // Added resource field
         },
         {
-          callcardNo: 'GHI789',
-          nature: 'Assault',
-          streetNo: '789',
-          level: 'Level 3',
-          street: 'Second Street',
-          precint: 'Precint4',
-          postalCode: '67890',
-          City: 'Puchong',
-          province: 'Selangor',
-          direction: 'East',
-          riskLevel: 'Low',
-          priority: 'Medium',
-          name: 'Dwayne Pebble',
-          phoneNo: '111-222-3333'
+            callcardNo: 'GHI789',
+            nature: 'Assault',
+            streetNo: '789',
+            level: 'Level 3',
+            street: 'Second Street',
+            precint: 'Precint4',
+            postalCode: '67890',
+            City: 'Puchong',
+            province: 'Selangor',
+            direction: 'East',
+            riskLevel: 'Low',
+            priority: 'Medium',
+            name: 'Dwayne Pebble',
+            phoneNo: '111-222-3333',
+            resource: 'FI 303' // Added resource field
         },
         {
-          callcardNo: 'JKL012',
-          nature: 'Traffic Accident',
-          streetNo: '012',
-          level: 'Level 4',
-          street: 'Third Avenue',
-          precint: 'Precint5',
-          postalCode: '09876',
-          City: 'Kuala Lumpur',
-          province: 'Kuala Lumpur',
-          direction: 'West',
-          riskLevel: 'Very High',
-          priority: 'Very High',
-          name: 'Raju Kumar Kamir',
-          phoneNo: '444-555-6666'
+            callcardNo: 'JKL012',
+            nature: 'Traffic Accident',
+            streetNo: '012',
+            level: 'Level 4',
+            street: 'Third Avenue',
+            precint: 'Precint5',
+            postalCode: '09876',
+            City: 'Kuala Lumpur',
+            province: 'Kuala Lumpur',
+            direction: 'West',
+            riskLevel: 'Very High',
+            priority: 'Very High',
+            name: 'Raju Kumar Kamir',
+            phoneNo: '444-555-6666',
+            resource: 'PO 757' // Added resource field
         }
-      ];
-      
+    ];
+    
+
 
     // Merge dummy incidents with existing incidents
     const mergedIncidents = [...incidentDataList, ...dummyIncidents];
@@ -104,7 +109,7 @@ const AllIncident = ({ incidentDataList, onCallCardClick }) => {
                         <tbody>
                             {mergedIncidents.map((incident, index) => (
                                 <tr key={index}>
-                                <td className="align-middle" onClick={() => handleCallCardClick(incident)} style={{ cursor: 'pointer' }}>{incident.callcardNo}</td>
+                                    <td className="align-middle" onClick={() => handleCallCardClick(incident)} style={{ cursor: 'pointer' }}>{incident.callcardNo}</td>
                                     <td className="align-middle">{incident.nature}</td>
                                     <td className="align-middle">
                                         {`No. ${incident.streetNo},
@@ -175,7 +180,7 @@ const getPriorityColor = (priority) => {
 
 const handleView = (incident) => {
     // Implement the logic to handle the view action here
-    
+
 };
 
 export default AllIncident;
